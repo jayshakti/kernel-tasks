400a07f0ab60c4e735bb025b184082acfa43d6de
jayshakti <jshakti528@gmail.com>
2021-07-21 09:45:42 +0530

    No change

--- print_kernel_logs/main.c
+++ print_kernel_logs/main.c
@@ -1,14 +1,16 @@
-
 #include<linux/module.h>
+
 
 static int __init helloKernel_init(void){
 	pr_info("Hello Kernel\n");
 	return 0;
 }
 
+
 static void __exit helloKernel_exit(void){
 	pr_info("Byee Byee Kernel\n");
 }
+
 
 // init & exit
 module_init(helloKernel_init);
@@ -19,4 +21,3 @@ module_exit(helloKernel_exit);
 MODULE_LICENSE("GPL");
 MODULE_AUTHOR("Me");
 MODULE_DESCRIPTION("Testing CDD to print kernel logs on the host machine");
-
