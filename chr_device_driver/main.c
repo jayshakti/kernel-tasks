c5ad1071c25f1db5d97b1da42d8319f64f20c538
jayshakti <jshakti528@gmail.com>
2021-07-21 23:22:47 +0530

    implementation of device register & unregister along with the file operations functions

---
+++ chr_device_driver/main.c
@@ -0,0 +1,63 @@
+#include"headers.h"
+#include"declarations.h"
+#include"fileOperations.h"
+
+int my_open(struct inode *inode, struct file *filp){
+	pr_info("Opening...\n");
+	return 0;
+}
+
+int my_release(struct inode *inode, struct file *filp){
+	pr_info("Closing...\n");
+	return 0;
+}
+
+ssize_t my_read(struct file *filp, char __user *buff, size_t count, loff_t *lfpos){
+	pr_info("Reading...\n");
+	return 0;
+}
+
+ssize_t my_write(struct file *filp, const char __user *buff, size_t count, loff_t *lfpos){
+	pr_info("Writing...\n");
+	return 0;
+}
+
+
+static int __init helloKernel_init(void){
+	pr_info("Hello Kernel\n");
+
+	//Allocating the device number
+	if(alloc_chrdev_region(&device_id, 0, 1, DEVICE_NAME) < 0){
+		pr_err("Can't allocate device: Errro!\n");
+	}
+	pr_info("Device registered successfully with...Major=%d & Minor=%d\n", MAJOR(device_id), MINOR(device_id));
+
+	// Initialising the device structure
+	cdev_init(&my_cdev, &fops);
+
+	//Adding the device into kernel device table
+	if(cdev_add(&my_cdev, device_id, 1) < 0){
+		pr_err("cdev_add() failed: Error!\n");
+		return -1;
+	}
+	return 0;
+}
+
+
+static void __exit helloKernel_exit(void){
+
+	//Un-allocating device number
+	unregister_chrdev_region(device_id, 1);
+	pr_info("Device un-registered successfully !! Byee Byee Kernel\n");
+}
+
+
+// init & exit
+module_init(helloKernel_init);
+module_exit(helloKernel_exit);
+
+
+// license
+MODULE_LICENSE("GPL");
+MODULE_AUTHOR("Me");
+MODULE_DESCRIPTION("Testing CDD to print kernel logs on the host machine");
