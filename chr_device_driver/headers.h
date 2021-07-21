7dcc0fc678a822997586ae94c1bf8a747d803bdd
jayshakti <jshakti528@gmail.com>
2021-07-21 23:19:09 +0530

    it contains all the header files used in the driver

---
+++ chr_device_driver/headers.h
@@ -0,0 +1,9 @@
+#include<linux/kernel.h>
+#include<linux/module.h>
+#include<linux/init.h>
+#include<linux/kdev_t.h>
+#include<linux/cdev.h>
+#include<linux/slab.h>
+#include<linux/uaccess.h>
+#include<linux/fs.h>
+#include<linux/device.h>
