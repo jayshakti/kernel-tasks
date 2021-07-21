7a0922567eb5c9f3720fa2f5e3425fcb8b68b1aa
jayshakti <jshakti528@gmail.com>
2021-07-21 15:17:53 +0530

    Implementation of IOCTL operation

---
+++ cdd_ioclt/my_ioctl.c
@@ -0,0 +1,92 @@
+#include<linux/init.h>
+#include<linux/module.h>
+#include<linux/kernel.h>
+#include<linux/fs.h>		// for file structure
+#include<linux/cdev.h> 		// for cdev structure
+#include<linux/kdev_t.h> 	// for device file
+#include<linux/device.h>
+#include<linux/slab.h>		// for kmalloc
+#include<linux/uaccess.h>	// for copy_to_user & copy_from_user
+#include<linux/ioctl.h>
+#include<linux/sched.h>		// for jiffies
+
+#define DEVICE_NAME "My_IOCTL_cdd"
+#define MAGICNO 'J'
+#define PRINT_JIFFIES _IO(MAGICNO, 'a')
+#define READ_JIFFIES _IOR(MAGICNO, 'b', unsigned long*)
+
+
+
+dev_t dev_num;
+static struct cdev my_cdev;
+
+static long my_ioctl(struct file *filp, unsigned int cmd, unsigned long arg){
+	unsigned long data = jiffies;
+	pr_info("Inside IOCTL kernel\n");
+	switch(cmd){
+		case READ_JIFFIES:
+			if(copy_to_user((unsigned long *)arg, &data, sizeof(data))){
+				pr_err("Jiffies Read: Error!\n");
+			}
+			break;
+		default:
+			pr_info("Printing jiffies inside kernel..%lu\n", data);
+	}
+	return 0;
+}
+
+// File operation structure
+static struct file_operations fops = {
+	.owner		= THIS_MODULE,
+	.unlocked_ioctl	= my_ioctl,
+};
+
+
+static int __init chr_init(void){
+
+	// Regestering/allocating the device number
+	if(alloc_chrdev_region(&dev_num, 0, 1, DEVICE_NAME) < 0){
+		pr_err("Can't allocate the device number: Error!\n");
+		return -1;
+	}
+	pr_info("Hello Kernel\n");
+	pr_info("Device registerd successfully with.... Major:%d and Minor:%d\n", MAJOR(dev_num), MINOR(dev_num));
+
+	// Initialising the device structure
+	cdev_init(&my_cdev, &fops);
+
+	
+/*	my_cdev = cdev_alloc();
+	if(my_cdev == NULL){
+		pr_err("cdev_alloc() failed: Error\n");
+		return -1;
+	}
+	my_cdev->owner = THIS_MODULE;
+	my_cdev->ops = &fops;
+*/
+	// Adding the device into the device table of the kernel
+	if(cdev_add(&my_cdev, dev_num, 1) < 0){
+		pr_err("cdev_add() failed: Error\n");
+		return -1;
+	}
+	return 0;
+
+}
+
+
+static void __exit chr_exit(void){
+	// Un-regestering/un-allocating the device number
+	unregister_chrdev_region(dev_num, 1);
+	pr_info("Device un-registered successfully !! Byee Kernel\n");
+}
+
+
+// init & exit macros
+module_init(chr_init);
+module_exit(chr_exit);
+
+
+// license
+MODULE_LICENSE("GPL");
+MODULE_AUTHOR("JST");
+MODULE_DESCRIPTION("Testing ioclt operations to read jiffies from kernel-space & print into user-space");
---
+++ cdd_ioclt/test_ioctl.c
@@ -0,0 +1,45 @@
+#include<unistd.h>
+#include<stdlib.h>
+#include<stdio.h>
+#include<sys/types.h>
+#include<sys/stat.h>
+#include<sys/ioctl.h>
+#include<fcntl.h>
+
+
+#define MAGICNO 'J'
+#define PRINT_JIFFIES _IO(MAGICNO, 'a')
+#define READ_JIFFIES _IOR(MAGICNO, 'b', unsigned long*)
+
+int main(){
+	int fd, ch;
+	unsigned long jiff = 0;
+	fd = open("/dev/chr_IOCTL",O_RDWR);
+	if(fd < 0){
+		perror("open");
+		printf("File can't be opened fd=%d\n", fd);
+		return -1;
+	}
+	printf("Application Begins: %s\n", __FILE__);
+	while(1){
+		printf("Press 1. to get jiffies from kernel-space into user-space\n");
+		printf("Press 2. to print jiffies in kernel space\n");
+		printf("Press 3. to exit from application\n");
+		scanf("%d", &ch);
+		if(ch == 1){
+			ioctl(fd, READ_JIFFIES, &jiff);
+			printf("Jiffies value: %lu\n", jiff);
+		}
+		else if(ch == 2){
+			ioctl(fd, PRINT_JIFFIES);
+		}
+		else{
+			break;
+		}
+	}
+
+	printf("Application Closed: %s\n", __FILE__);
+	close(fd);
+	return 0;
+
+}
